a3736115cf963f0f065fba0627a473b2bd289cb0
Steve Hocking <steve@codemunki.es>
2021-10-11 21:08:19 +0100

    Add another expetation setting test

--- test/BusinessRules.UnitTests/Entities/PackingSlipTests.cs
+++ test/BusinessRules.UnitTests/Entities/PackingSlipTests.cs
@@ -48,5 +48,19 @@ namespace BusinessRules.UnitTests.Entities
             // Assert
             result.Should().BeFalse();
         }
+
+        [Fact]
+        public void GivenAPackingSlip_WhenThereAreMultipleItems_ThenIsTypePresentCorrectlyRespondsForRelatedType()
+        {
+            // Arrange
+            var productList = new List<BaseProduct> { new PhysicalProduct(), new BookProduct(), new PhysicalProduct() };
+            var subject = new PackingSlip { Product = productList.AsReadOnly() };
+
+            // Act
+            var result = subject.ContainsProductType<PhysicalProduct>();
+
+            // Assert
+            result.Should().BeTrue();
+        }
     }
 }
