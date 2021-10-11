5927e9acead67c3f6915393a9c6c3ef5262291a3
Steve Hocking <steve@codemunki.es>
2021-10-11 16:40:04 +0100

    Add a unit test project

---
+++ test/BusinessRules.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BusinessRules.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
