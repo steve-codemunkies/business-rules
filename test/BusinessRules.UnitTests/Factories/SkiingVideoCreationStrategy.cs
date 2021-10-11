5a61ec6df55158e067442f133286528c0765967e
Steve Hocking <steve@codemunki.es>
2021-10-11 22:15:27 +0100

    Make failing test pass

--- test/BusinessRules.UnitTests/Factories/SkiingVideoCreationStrategy.cs
+++ test/BusinessRules.UnitTests/Factories/SkiingVideoCreationStrategy.cs
@@ -51,6 +51,11 @@ namespace BusinessRules.UnitTests.Factories
     {
         public void Apply(IList<BaseProduct> baseProducts)
         {
+            if(!baseProducts.Any(bp => bp is VideoProduct && string.Compare(((VideoProduct)bp).Title, "Learning to Ski", StringComparison.OrdinalIgnoreCase) == 0))
+            {
+                return;
+            }
+
             baseProducts.Add(new VideoProduct { Title = "First Aid" });
         }
     }
