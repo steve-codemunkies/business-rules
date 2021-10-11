fd0fade46962281eb2461c999c6be4bef7bcc495
Steve Hocking <steve@codemunki.es>
2021-10-11 21:45:33 +0100

    Add failing test for factory with no strategies

--- test/BusinessRules.UnitTests/Factories/PackingSlipFactoryTests.cs
+++ test/BusinessRules.UnitTests/Factories/PackingSlipFactoryTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using BusinessRules.Entities;
+using BusinessRules.Factories;
 using FluentAssertions;
 using Xunit;
 
@@ -7,6 +9,8 @@ namespace BusinessRules.UnitTests.Factories
 {
     public class PackingSlipFactoryTests
     {
+        public class TestProduct : BaseProduct { }
+
         [Fact]
         public void GivenIAmConstructingPackingSlipFactory_WhenIDoNotSupplyCreationStrategies_ThenAnExecptionIsThrown()
         {
@@ -18,15 +22,36 @@ namespace BusinessRules.UnitTests.Factories
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("creationStrategies");
         }
+
+        [Fact]
+        public void GivenNoStrategies_WhenAnOrderIsProcessed_ThenAPackingSlipIsCreated()
+        {
+            // Arrange
+            IPackingSlipFactory subject = new PackingSlipFactory(new ICreationStrategy[0]);
+            var order = new Order { Product = new TestProduct() };
+
+            // Act
+            var result = subject.BuildPackingSlip(order);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Product.Should().NotBeNullOrEmpty();
+            result.Product.Should().Contain(order.Product);
+        }
     }
 
-    public class PackingSlipFactory
+    public class PackingSlipFactory : IPackingSlipFactory
     {
         private IEnumerable<ICreationStrategy> _creationStrategies;
 
         public PackingSlipFactory(IEnumerable<ICreationStrategy> creationStrategies)
         {
             _creationStrategies = creationStrategies ?? throw new ArgumentNullException(nameof(creationStrategies));
+        }
+
+        public PackingSlip BuildPackingSlip(Order order)
+        {
+            throw new NotImplementedException();
         }
     }
 
