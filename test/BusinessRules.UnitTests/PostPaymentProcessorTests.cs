97eeebc3e04cda3e1f6d308b3a4763b5fb7790a7
Steve Hocking <steve@codemunki.es>
2021-10-11 20:37:41 +0100

    Add a passing test for calling the rules

--- test/BusinessRules.UnitTests/PostPaymentProcessorTests.cs
+++ test/BusinessRules.UnitTests/PostPaymentProcessorTests.cs
@@ -1,11 +1,17 @@
 using System;
+using BusinessRules.Entities;
+using BusinessRules.Rules;
 using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Moq;
 using Xunit;
 
 namespace BusinessRules.UnitTests
 {
     public class PostPaymentProcessorTests
     {
+        public class TestProduct : BaseProduct { }
+
         [Fact]
         public void GivenThatIAmConstructingThePostPaymentProcessor_WhenIDoNotSupplyRuleStrategies_ThenAnExceptionIsThrown()
         {
@@ -16,6 +22,26 @@ namespace BusinessRules.UnitTests
             // Assert
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("ruleStrategies");
+        }
+
+        [Fact]
+        public void GivenAnOrder_WhenProcessingTheOrder_ThenTheOrderIsPassedToAllRules()
+        {
+            // Arrange
+            var ruleMock1 = new Mock<IRuleStrategy>();
+            var ruleMock2 = new Mock<IRuleStrategy>();
+            var ruleMock3 = new Mock<IRuleStrategy>();
+            var subject = new PostPaymentProcessor(new[] { ruleMock1.Object, ruleMock2.Object, ruleMock3.Object });
+
+            var order = new Order { Product = new TestProduct() };
+
+            // Act
+            subject.Process(order);
+
+            // Assert
+            ruleMock1.Verify(r => r.ApplyRule(It.Is<PackingSlip>(ps => ps.Product == order.Product)), Times.Once);
+            ruleMock2.Verify(r => r.ApplyRule(It.Is<PackingSlip>(ps => ps.Product == order.Product)), Times.Once);
+            ruleMock3.Verify(r => r.ApplyRule(It.Is<PackingSlip>(ps => ps.Product == order.Product)), Times.Once);
         }
     }
 }
