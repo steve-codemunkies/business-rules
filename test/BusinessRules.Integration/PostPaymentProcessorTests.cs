c8c4e40e8c8bee40b49a9f173a9698ad6ac7746b
Steve Hocking <steve@codemunki.es>
2021-10-11 22:30:03 +0100

    Add failing test for skiing video

--- test/BusinessRules.Integration/PostPaymentProcessorTests.cs
+++ test/BusinessRules.Integration/PostPaymentProcessorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using BusinessRules.Entities;
 using BusinessRules.External;
 using BusinessRules.Factories;
@@ -96,6 +97,36 @@ namespace BusinessRules.Integration
             shippingMock.Verify(s => s.ShipIt(It.IsAny<PackingSlip>()), Times.Never);
             royaltyDepartmentMock.Verify(rd => rd.ProcessRoyalties(It.IsAny<PackingSlip>()), Times.Never);
             memberServicesMock.Verify(ms => ms.Activate(membership), Times.Once);
+        }
+
+        [Fact]
+        public void GivenAPaymentHasBeenCompleted_WhenTheProductIsAVideoTitledLearningToSki_ThenTheAFirstAidVideoIsAddedAndShipped()
+        {
+            // Arrange
+            var shippingMock = new Mock<IShipping>();
+            var royaltyDepartmentMock = new Mock<IRoyaltyDepartment>();
+            var memberServicesMock = new Mock<IMemberServices>();
+            var subject = BuildPostPaymentProcessor(shippingMock.Object, royaltyDepartmentMock.Object, memberServicesMock.Object);
+
+            var video = new VideoProduct { Title = "Learning to Ski" };
+            var order = new Order { Product = video };
+
+            PackingSlip shippedPackingSlip = null;
+            shippingMock.Setup(s => s.ShipIt(It.IsAny<PackingSlip>()))
+                .Callback((PackingSlip ps) => shippedPackingSlip = ps);
+
+            // Act
+            subject.Process(order);
+
+            // Assert
+            royaltyDepartmentMock.Verify(rd => rd.ProcessRoyalties(It.IsAny<PackingSlip>()), Times.Never);
+            memberServicesMock.Verify(ms => ms.Activate(It.IsAny<Membership>()), Times.Never);
+
+            shippedPackingSlip.Should().NotBeNull();
+            shippedPackingSlip.Product.Should().NotBeNullOrEmpty();
+            shippedPackingSlip.Product.Count.Should().Be(2);
+            shippedPackingSlip.Product.Should().Contain(bp => bp is VideoProduct && string.Compare(((VideoProduct)bp).Title, "First Aid", StringComparison.OrdinalIgnoreCase) == 0);
+            shippedPackingSlip.Product.Should().Contain(bp => bp is VideoProduct && string.Compare(((VideoProduct)bp).Title, "Learning to Ski", StringComparison.OrdinalIgnoreCase) == 0);
         }
     }
 }
