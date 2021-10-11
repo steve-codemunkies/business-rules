a74b17c34e8792ccf8142cd6d67e36589f0093f2
Steve Hocking <steve@codemunki.es>
2021-10-11 21:40:06 +0100

    Make failing test pass

--- test/BusinessRules.UnitTests/Factories/PackingSlipFactoryTests.cs
+++ test/BusinessRules.UnitTests/Factories/PackingSlipFactoryTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
@@ -17,5 +18,19 @@ namespace BusinessRules.UnitTests.Factories
             act.Should().Throw<ArgumentNullException>()
                 .WithParameterName("creationStrategies");
         }
+    }
+
+    public class PackingSlipFactory
+    {
+        private IEnumerable<ICreationStrategy> _creationStrategies;
+
+        public PackingSlipFactory(IEnumerable<ICreationStrategy> creationStrategies)
+        {
+            _creationStrategies = creationStrategies ?? throw new ArgumentNullException(nameof(creationStrategies));
+        }
+    }
+
+    public interface ICreationStrategy
+    {
     }
 }
