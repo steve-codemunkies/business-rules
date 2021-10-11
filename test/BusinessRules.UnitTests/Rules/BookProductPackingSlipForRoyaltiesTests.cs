943d4061161ed1ebbd85283215b6a9b30c06dcfd
Steve Hocking <steve@codemunki.es>
2021-10-11 17:13:44 +0100

    Add a failing test for processing book purchases

---
+++ test/BusinessRules.UnitTests/Rules/BookProductPackingSlipForRoyaltiesTests.cs
@@ -0,0 +1,27 @@
+using BusinessRules.Entities;
+using BusinessRules.External;
+using BusinessRules.Rules;
+using Moq;
+using Xunit;
+
+namespace BusinessRules.UnitTests.Rules
+{
+    public class BookProductPackingSlipForRoyaltiesTests
+    {
+        [Fact]
+        public void GivenAPackingSlipToProcess_WhenTheSlipContainsABookProduct_ThenTheRoyaltiesDepartmentIsCalled()
+        {
+            // Arrange
+            var royaltiesDepartmentMock = new Mock<IRoyaltyDepartment>();
+            IRuleStrategy subject = new BookProductPackingSlipForRoyalties(royaltiesDepartmentMock.Object);
+
+            var packingSlip = new PackingSlip { Product = new BookProduct() };
+
+            // Act
+            subject.ApplyRule(packingSlip);
+
+            // Assert
+            royaltiesDepartmentMock.Verify(s => s.ProcessRoyalties(packingSlip), Times.Once);
+        }
+    }
+}
